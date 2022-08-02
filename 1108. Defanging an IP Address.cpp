b6876c2441401c4aee5d8a256fc20a4ec110db85
AsitKumar0610 <77540139+AsitKumar0610@users.noreply.github.com>
2022-08-02 14:44:14 +0530

    Create 1108. Defanging an IP Address.cpp

---
+++ 1108. Defanging an IP Address.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string str = "" ;
+        for(auto c : address)
+        {if(c == '.'){   str += "[.]" ;   }
+         else {   str += c ;   }
+        }return str;
+    }
+};

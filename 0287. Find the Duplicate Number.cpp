c3561409ae05d16742254f7713ddc0c79f392037
Asit Kumar <77540139+AsitKumar0610@users.noreply.github.com>
2022-09-12 16:15:14 +0530

    Create 0287. Find the Duplicate Number.cpp

---
+++ 0287. Find the Duplicate Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i = 0 ;
+        while(nums[i] != nums[i+1]){
+        i++;
+        }
+        return nums[i];
+    }
+};

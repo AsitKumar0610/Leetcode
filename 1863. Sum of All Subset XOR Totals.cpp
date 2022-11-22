80a0a3a855f85671081c35aad68822f3d71ffce9
Asit Kumar <77540139+AsitKumar0610@users.noreply.github.com>
2022-11-22 17:15:10 +0530

    Create 1863. Sum of All Subset XOR Totals.cpp

---
+++ 1863. Sum of All Subset XOR Totals.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sum=0;
+    void solve(vector<int>& nums,int i,int xr){
+        if(i==nums.size()) sum+=xr;
+        else {
+            solve(nums,i+1,xr^nums[i]);
+            solve(nums,i+1,xr);
+        }
+        
+    }
+    int subsetXORSum(vector<int>& nums) {
+        solve(nums,0,0);
+        return sum;
+    }
+};

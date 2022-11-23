813e2be8cdfd59b8de8b98b7b67b5ac2f1317fd7
Asit Kumar <77540139+AsitKumar0610@users.noreply.github.com>
2022-11-23 14:37:11 +0530

    Create 2441. Largest Positive Integer That Exists With Its Negative.cpp

---
+++ 2441. Largest Positive Integer That Exists With Its Negative.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+    int ans=-1;
+    for(auto i:nums)
+    {
+        for(auto j:nums)
+        {
+            if(i==-j) ans=max(ans,abs(i));
+        }
+    }
+    return ans;
+}
+};

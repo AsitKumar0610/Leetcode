135fe51854ed6b789bc7ffbc0f645633bd0ed9f4
Asit Kumar <77540139+AsitKumar0610@users.noreply.github.com>
2022-11-23 11:52:01 +0530

    Create 0896. Monotonic Array.cpp

---
+++ 0896. Monotonic Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int inc = 1, dec = 1 ;
+        for(int i = 1 ; i < nums.size() ; i++){
+            if(nums[i] >= nums[i-1])  inc++;
+            if(nums[i] <= nums[i-1])  dec++;
+        }
+        if(inc == nums.size() || dec == nums.size()){   return true;  }
+        return false;
+    }
+};

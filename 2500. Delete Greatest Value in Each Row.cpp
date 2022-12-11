fc888b349b20c71a21a91c6aae2c390c977c9a13
Asit Kumar <77540139+AsitKumar0610@users.noreply.github.com>
2022-12-11 20:34:02 +0530

    Create 2500. Delete Greatest Value in Each Row.cpp

---
+++ 2500. Delete Greatest Value in Each Row.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int deleteGreatestValue(vector<vector<int>>& grid) {
+        
+        for(int i=0; i<grid.size(); i++){
+            sort(grid[i].rbegin(), grid[i].rend());
+        }
+        
+        int ans = 0;
+        for(int i=0; i<grid[0].size(); i++){
+            int val = grid[0][i];
+            for(int j=1; j<grid.size(); j++){
+                val = max(val, grid[j][i]);
+            }
+            ans += val;
+        }
+        return ans;
+    }
+};
